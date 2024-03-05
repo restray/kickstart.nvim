32a92a78693fb3c6b87b7b5274c2067082edf3c9
Timothée Belhomme <timothee.belhomme@neomi.immo>
2024-03-05 17:09:08 +0100

    add: auto-format vue & javascript

--- init.lua
+++ init.lua
@@ -656,7 +656,8 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'eslint_d', 'prettierd', 'prettier' } },
+        vue = { { 'eslint_d', 'prettierd', 'prettier' } },
       },
     },
   },
