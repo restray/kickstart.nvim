f9f9ba439521f37285b6258bbe2fa186eb78d653
restray <contact@restray.org>
2025-04-11 14:27:33 +0200

    add base64

---
+++ lua/custom/plugins/base64.lua
@@ -0,0 +1,11 @@
+return {
+  'taybart/b64.nvim',
+  config = function()
+    vim.keymap.set('x', '<leader>be', function()
+      require("b64").encode()
+    end, { desc = '[B]ase64 [e]ncode' })
+    vim.keymap.set('x', '<leader>bd', function()
+      require("b64").decode()
+    end, { desc = '[B]ase64 [d]ecode' })
+  end,
+}
