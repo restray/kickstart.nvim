ffd9561eb271575502c602606c109b8cc44ba624
Restray <contact@restray.org>
2025-05-12 10:33:19 +0200

    fix mason errors

--- init.lua
+++ init.lua
@@ -478,12 +478,13 @@ require('lazy').setup({
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
+    version = "1.*",
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'mason-org/mason.nvim', opts = {} },
-      'mason-org/mason-lspconfig.nvim',
+      { 'mason-org/mason.nvim', version = "1.*", opts = {} },
+      {'mason-org/mason-lspconfig.nvim', version = "1.*"},
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
