162baf3caaf5e768e65bdb6f3ac3f12af49ce805
Restray <27992458+restray@users.noreply.github.com>
2025-10-04 11:01:25 +0200

    fix(copilot): remove mcps

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,29 +1,3 @@
 return {
-  {
-    'deathbeam/CopilotChat.nvim',
-    dependencies = {
-      { 'zbirenbaum/copilot.lua' },
-      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-    },
-    branch = 'tools',
-    build = 'make tiktoken',
-    config = function()
-      require 'CopilotChat'.setup()
-    end,
-  },
-  {
-    'zbirenbaum/copilot.lua',
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          auto_trigger = true,
-          keymap = {
-            accept = "<S-Tab>",
-          },
-        }
-      })
-    end,
-  }
+  'github/copilot.vim',
 }
